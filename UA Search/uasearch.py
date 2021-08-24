51674e2e23098e1ca7d5455d4101b9707e4e6c7b
Nyarlathotep <30965989+SimplyATable@users.noreply.github.com>
2021-08-24 15:40:41 -0700

    Changed "Items" to "results"

--- UA Search/uasearch.py
+++ UA Search/uasearch.py
@@ -2,4 +2,4 @@
 {{s=s[0] if s and (len(s)==1 or s[0].name.lower()==n) else [x.name for x in g] if not n else [x.name for x in s]}}
 {{raw=q.join(s)}}{{raw=raw[:1020]+'...' if len(raw)>1024 else raw}}
 -title "Fight Club: Allowed UA"
-{{(f' -title "{s.name}" -desc "" -f "Description|{s.desc}" -footer "Type: {s.type}"') if 'name' in s else f'-f "List of Items containing {n}|{raw if s else "No Matches Found"}"' if n else '-desc "`!testua [search]` to look up something.\n[Source](https://bit.ly/3u73Iu7)"'}}
+{{(f' -title "{s.name}" -desc "" -f "Description|{s.desc}" -footer "Type: {s.type}"') if 'name' in s else f'-f "List of results containing {n}|{raw if s else "No Matches Found"}"' if n else '-desc "`!testua [search]` to look up something.\n[Source](https://bit.ly/3u73Iu7)"'}}
