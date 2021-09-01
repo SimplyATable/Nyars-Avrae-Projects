ef3000417eaf578ab45bd50ee359ba9393fd4ee9
Nyarlathotep <30965989+SimplyATable@users.noreply.github.com>
2021-08-31 19:15:21 -0700

    Modified the program to work with Atom

--- UA Search/automated_desc.py
+++ UA Search/automated_desc.py
@@ -1,9 +1,10 @@
-text = input("Your text here: ")
+text = ""
 list = text.split('|')
 finaltext = []
 for x in list:
     finaltext.append(f"""\\n\\" -f \\"{x}""")
-print(' '.join(str(i) for i in finaltext).replace('\n', ''))
-
+final = ''.join(str(i) for i in finaltext).replace('\n', '')
+print (f"""{final}
+""")
 # Must put | between sections of text. Must put | and \n in as needed after running
 # CHECK RESULT AFTER USING
