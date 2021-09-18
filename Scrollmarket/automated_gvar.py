bbbebc87f6d9e75b68885a23a0f75d6cfd49153b
Nyarlathotep <30965989+SimplyATable@users.noreply.github.com>
2021-09-18 01:42:13 -0700

    updated

--- Scrollmarket/automated_gvar.py
+++ Scrollmarket/automated_gvar.py
@@ -2,6 +2,6 @@ text = """"""
 list = text.split('\n')
 finaltext = []
 for x in list:
-    finaltext.append(f"""{{"level":"0","name":\"""" + x + """\"},""")
-final = ''.join(str(i) for i in finaltext)
+    finaltext.append(f"""{{"level":"9","name":\"""" + x + """\"},""")
+final = ''.join(str('\n'+i) for i in finaltext)
 print (f"""{final}""")
