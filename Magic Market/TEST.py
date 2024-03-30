92dcbd5ef452f78da5f4bb28e8bd13206440b27c
Nyarlathotep <30965989+SimplyATable@users.noreply.github.com>
2024-03-30 01:12:06 -0700

    Banned Chronurgy Wizard and Spells

---
+++ Magic Market/TEST.py
@@ -0,0 +1,51 @@
+# text = """Astral Traveler
+# Enchantments
+# Fey Bargains
+# Magic Items
+# Magical Creatures
+# Necromancy
+# Needful Things
+# Time-Lost"""
+# list = text.split("""
+# """)
+# finaltext = []
+# for x in list:
+#     finaltext.append(f"""        "{x} (Medium)",
+#         "{x} (Medium)",
+#         "{x} (Good)",
+#         "{x} (Good)",
+#         "{x} (Excellent)",
+# """)
+# final = ''.join(str(i) for i in finaltext)
+# print (f"""{final}
+# """)
+
+
+
+
+# text = """Leatherworking
+# Mechanical Contraptions
+# Medium and Heavy Armor
+# Potions, Poisons, and Herbs
+# Religious Idols and Blessing
+# Songs and Instruments
+# Spell Scrolls and Tomes
+# Tattoos
+# Tools
+# Weapons
+# Witchcraft"""
+# list = text.split("""
+# """)
+# finaltext = []
+# for x in list:
+#     finaltext.append(f"""    "{x}":[
+#         "{x} (Medium)",
+#         "{x} (Medium)",
+#         "{x} (Good)",
+#         "{x} (Good)",
+#         "{x} (Excellent)",
+#     ],
+#     """)
+# final = ''.join(str(i) for i in finaltext)
+# print (f"""{final}
+# """)
